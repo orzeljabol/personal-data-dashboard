6e7cf0ec453f24b58eab4ba37e63bbc4998bfd61
orzeljabol <zebrowskibartek7@gmail.com>
2026-06-26 10:23:10 +0200

    Fix average summary calculations

--- app/services.py
+++ app/services.py
@@ -12,31 +12,37 @@ def get_last_7_days_entries(db: Session = Depends(get_db)):
     return entries
 def calculate_average_sleep_hours(entries):
     total_sleep = 0
+    days = 0
     for entry in entries:
         if entry.sleep_hours is not None:
             total_sleep += entry.sleep_hours
-    if entries:
-        average_sleep = total_sleep / len(entries)
+            days += 1
+    if days > 0:
+        average_sleep = total_sleep / days
     else:
         return 0
     return average_sleep
 def calculate_average_mood(entries):
     total_mood = 0
+    days = 0
     for entry in entries:
         if entry.mood is not None:
             total_mood += entry.mood
-    if entries:
-        average_mood = total_mood / len(entries)
+            days += 1
+    if days > 0:
+        average_mood = total_mood / days
     else:
         return 0
     return average_mood
 def calculate_average_energy(entries):
     total_energy = 0
+    days = 0
     for entry in entries:
         if entry.energy is not None:
             total_energy += entry.energy
-    if entries:
-        average_energy = total_energy / len(entries)
+            days += 1
+    if days > 0:
+        average_energy = total_energy / days
     else:
         return 0
     return average_energy
@@ -48,22 +54,26 @@ def calculate_total_deep_work_minutes(entries):
     return total_deep_work
 def calculate_average_deep_work_minutes(entries):
     total_deep_work = 0
+    days = 0
     for entry in entries:
         if entry.deep_work_minutes is not None:
             total_deep_work += entry.deep_work_minutes
-    if entries:
-        average_deep_work = total_deep_work / len(entries)
+            days += 1
+    if days > 0:
+        average_deep_work = total_deep_work / days
     else:
         return 0
     return average_deep_work
 def calculate_average_exercise_minutes(entries):
 
     total_exercise = 0
+    days = 0    
     for entry in entries:
         if entry.exercise_minutes is not None:
             total_exercise += entry.exercise_minutes
-    if entries:
-        average_exercise = total_exercise / len(entries)
+            days += 1
+    if days > 0:
+        average_exercise = total_exercise / days
     else:
         return 0
     return average_exercise
@@ -75,11 +85,13 @@ def calculate_total_exercise_minutes(entries):
     return total_exercise
 def calculate_average_stimulation_minutes(entries):
     total_stimulation = 0
+    days = 0
     for entry in entries:
         if entry.stimulation_minutes is not None:
             total_stimulation += entry.stimulation_minutes
-    if entries:
-        average_stimulation = total_stimulation / len(entries)
+            days += 1
+    if days > 0:
+        average_stimulation = total_stimulation / days
     else:
         return 0
     return average_stimulation
@@ -91,11 +103,13 @@ def calculate_total_stimulation_minutes(entries):
     return total_stimulation
 def calculate_average_litres_water(entries):
     total_water = 0
+    days = 0
     for entry in entries:
         if entry.litres_water is not None:
             total_water += entry.litres_water
-    if entries:
-        average_water = total_water / len(entries)
+            days += 1
+    if days > 0:
+        average_water = total_water / days
     else:
         return 0
     return average_water
