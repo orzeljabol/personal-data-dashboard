c5c2a3f0a5da4cf0772995b5b4ef60affc15cb4b
orzeljabol <zebrowskibartek7@gmail.com>
2026-07-16 08:07:32 +0200

    test: expand API integration coverage

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-def test_create_entry_can_be_retrived(client):
+def test_create_entry_can_be_retrieved(client):
     payload = {
         "date": "2026-07-20",
         "mood": 5,
@@ -45,8 +45,7 @@ def test_create_entry_duplicate_date(client):
     assert second_response.status_code == 400
 
 
-def test_create_entry_invalid_data(client):
-    
+def test_create_entry_invalid_data(client): 
     payload = {
         "date": "2026-07-20",
         "mood": 11,
@@ -63,3 +62,55 @@ def test_create_entry_invalid_data(client):
     
     assert response.status_code == 200
     assert response.json() == []
+
+
+def test_empty_database(client):
+    response = client.get("/api/entries")
+    
+    assert response.status_code == 200
+    assert response.json()== []
+
+
+def test_get_entries_returns_multiple_entries_in_descending_date_order(client):
+    payload1 = {
+        "date": "2026-07-20",
+        "mood": 5,
+        "energy": 6
+    }
+    response_post1 = client.post(
+        "/api/entries/",
+        json=payload1
+    )
+    
+    assert response_post1.status_code == 201
+
+    payload2 = {
+        "date": "2026-07-21",
+        "mood": 7,
+        "energy": 8
+    }
+    response_post2 = client.post(
+        "/api/entries/",
+        json=payload2
+    )
+    
+    assert response_post2.status_code == 201
+
+    response_get = client.get("/api/entries")
+
+    assert response_get.status_code == 200
+    data = response_get.json()
+    
+    assert len(data) == 2
+    
+    newest_entry = data[0]
+    oldest_entry = data[1]
+    
+    
+    assert newest_entry["date"] == "2026-07-20"
+    assert newest_entry["mood"] == 5
+    assert newest_entry["energy"] == 6
+    
+    assert oldest_entry["date"] == "2026-07-21"
+    assert oldest_entry["mood"] == 7
+    assert oldest_entry["energy"] == 8
